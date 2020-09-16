246130118c82d64f5200a6fc2e3fec93396cb631
ricardovsoares <ricardovsoares@gmail.com>
2020-09-16 16:49:56 -0300

    Adicionando Componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <h1>Hello Semana DevSuperior</h1>
+    <div>
+        <Header />
+    </div>
   );
 }
 
